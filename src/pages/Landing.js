0cc389356a09e16a5d6a08fc2d08b6ed809b5264
Otabek Rakhimov <rahimovmail@gmail.com>
2023-02-17 18:54:23 -0800

    Add basic setups for landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,30 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
-  return <h1>Landing page</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
+            tempore nulla repudiandae laudantium cupiditate quia neque corrupti
+            laboriosam consequuntur impedit!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
