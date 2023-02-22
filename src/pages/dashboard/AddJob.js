c06e4b15c4c6569017691fdfb0fae2d383d128a8
Otabek Rakhimov <rahimovmail@gmail.com>
2023-02-22 13:49:11 -0800

    Create job request function

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -2,7 +2,11 @@ import { FormRow, FormRowSelect } from "../../components";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
-import { handleChange } from "../../features/job/jobSlice";
+import {
+  handleChange,
+  clearValues,
+  createJob,
+} from "../../features/job/jobSlice";
 
 const AddJob = () => {
   const {
@@ -26,6 +30,7 @@ const AddJob = () => {
       toast.error("Please Fill Out All Fields");
       return;
     }
+    dispatch(createJob({ position, company, jobLocation, jobType, status }));
   };
   const handleJobInput = (e) => {
     const name = e.target.name;
@@ -83,7 +88,7 @@ const AddJob = () => {
             <button
               type="button"
               className="btn btn-block clear-btn"
-              onClick={() => console.log("clear values")}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
